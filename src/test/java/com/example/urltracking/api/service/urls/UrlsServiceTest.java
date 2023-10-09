fba679971d8e7686c4db99fb4ba5bbb14b1c4791
bhoon <toltolyibh@gmail.com>
2023-10-09 12:37:13 +0900

    test: getUrlCount service 테스트 작성

--- src/test/java/com/example/urltracking/api/service/urls/UrlsServiceTest.java
+++ src/test/java/com/example/urltracking/api/service/urls/UrlsServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.urltracking.api.service.urls;
 
 import com.example.urltracking.CommonServiceTest;
+import com.example.urltracking.api.service.urls.request.UrlCountServiceRequest;
 import com.example.urltracking.api.service.urls.request.UrlCreateServiceRequest;
 import com.example.urltracking.api.service.urls.request.UrlUpdateServiceRequest;
+import com.example.urltracking.api.service.urls.response.UrlCountResponse;
 import com.example.urltracking.api.service.urls.response.UrlCreateResponse;
 import com.example.urltracking.api.service.urls.response.UrlUpdateResponse;
 import com.example.urltracking.exception.CustomException;
@@ -92,25 +94,25 @@ class UrlsServiceTest extends CommonServiceTest {
 
         //then
         assertNotNull(response);
-        assertEquals(request.getUrl(), response.getUrl());
+        assertThat(request.getUrl()).isEqualTo(response.getUrl());
         assertThat(request.getDailyCount() + 1).isEqualTo(response.getDailyCount());
         assertThat(request.getTotalCount() + 1).isEqualTo(response.getTotalCount());
     }
 
-//    @DisplayName("url의 조회수를 증가시킬 때 url이 등록되어있지 않으면 예외가 발생한다")
-//    @Test
-//    void get_url_count() {
-//        // given
-//        UrlUpdateServiceRequest request = UrlUpdateServiceRequest.builder()
-//                .url("www.abc.com/1/2")
-//                .trackingUrl("www.abc.com/123123")
-//                .dailyCount(0)
-//                .totalCount(0)
-//                .build();
-//
-//        // when // then
-//        assertThatThrownBy(() -> urlsService.updateUrlCount(request))
-//                .isInstanceOf(CustomException.class)
-//                .hasMessage("VALID URL IS NOT EXIST");
-//    }
+    @DisplayName("url로 오늘/누적 조회수를 조회한다")
+    @Test
+    void get_url_count() {
+        // given
+        UrlCountServiceRequest request = UrlCountServiceRequest.builder()
+                .trackingUrl("https://make.my.url/2")
+                .build();
+
+        // when
+        UrlCountResponse response = urlsService.getUrlCount(request);
+
+        // then
+        assertNotNull(response);
+        assertThat(response.getDailyCount()).isEqualTo(0);
+        assertThat(response.getTotalCount()).isEqualTo(3);
+    }
 }
