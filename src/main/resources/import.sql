08a724c6e3108345ee57365c54479482ce0f249d
owner <toltolyibh@gmail.com>
2023-10-09 22:45:56 +0900

    test: setUp 메서드 삭제
    setUp 메서드 삭제 및 import.sql에서 데이터 세팅

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,12 @@
-insert into urls(id_deleted,total_count,tracking_url,url) values (false,35,'https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into urls(id_deleted,total_count,tracking_url,url) values (false,5,'https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5742','localhost://a/2');
-insert into urls(id_deleted,total_count,tracking_url,url) values (false,2,'https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5743','localhost://a/3');
-insert into urls(id_deleted,total_count,tracking_url,url) values (false,1,'https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5744','localhost://a/4');
+insert into urls(id_deleted,total_count,url,tracking_url) values (false,5,'localhost://test/1','https://make.my.url/1');
+insert into urls(id_deleted,total_count,url,tracking_url) values (false,3,'localhost://test/2','https://make.my.url/2');
+insert into urls(id_deleted,total_count,url,tracking_url) values (false,4,'localhost://test/3','https://make.my.url/3');
+insert into urls(id_deleted,total_count,url,tracking_url) values (false,5,'localhost://test/4','https://make.my.url/4');
 
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (8,'2023-10-01','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (2,'2023-10-02','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (3,'2023-10-03','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (7,'2023-10-04','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (4,'2023-10-05','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (2,'2023-10-06','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (3,'2023-10-07','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (6,'2023-10-08','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5741','localhost://a/1');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (0,'2023-10-08','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5742','localhost://a/2');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (2,'2023-10-08','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5743','localhost://a/3');
-insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (1,'2023-10-08','https://make.my.url/4adb9c62-a01c-498e-9d18-89516cfd5744','localhost://a/4');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (3,'2023-10-01','localhost://test/1','https://make.my.url/1');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (3,'2023-10-02','localhost://test/1','https://make.my.url/1');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (2,'2023-10-03','localhost://test/1','https://make.my.url/1');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (3,'2023-10-08','localhost://test/2','https://make.my.url/2');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (4,'2023-10-08','localhost://test/3','https://make.my.url/3');
+insert into daily_count(daily_count,date,daily_tracking_url,daily_url) values (5,CURDATE(),'localhost://test/4','https://make.my.url/4');
 
--- src/test/java/com/example/urltracking/CommonRepositoryTest.java
+++ src/test/java/com/example/urltracking/CommonRepositoryTest.java
@@ -37,21 +37,4 @@ public abstract class CommonRepositoryTest {
                 .dailyCount(count)
                 .build();
     }
-
-    @BeforeEach
-    protected void setUp() {
-        Urls url1 = createUrls("localhost://test/1","https://make.my.url/1",5);
-        Urls url2 = createUrls("localhost://test/2","https://make.my.url/2",3);
-        Urls url3 = createUrls("localhost://test/3","https://make.my.url/3",4);
-        Urls url4 = createUrls("localhost://test/4","https://make.my.url/4",5);
-        urlsRepository.saveAll(List.of(url1, url2, url3, url4));
-
-        DailyCount count1 = createDailyCount("localhost://test/1", "https://make.my.url/1", LocalDate.of(2023,10,1),3);
-        DailyCount count2 = createDailyCount("localhost://test/1", "https://make.my.url/1", LocalDate.of(2023,10,8),3);
-        DailyCount count3 = createDailyCount("localhost://test/1", "https://make.my.url/1",LocalDate.of(2023,10,9),2);
-        DailyCount count4 = createDailyCount("localhost://test/2","https://make.my.url/2",LocalDate.of(2023,10,7),3);
-        DailyCount count5 = createDailyCount("localhost://test/3","https://make.my.url/3",LocalDate.of(2023,10,7),4);
-        DailyCount count6 = createDailyCount("localhost://test/4","https://make.my.url/4",LocalDate.now(),5);
-        dailyCountRepository.saveAll(List.of(count1, count2, count3, count4, count5, count6));
-    }
 }
