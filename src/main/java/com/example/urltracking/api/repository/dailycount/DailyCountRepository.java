043a63162c8ed85fb93d81d41c93c9ff46dfa250
owner <toltolyibh@gmail.com>
2023-10-10 20:03:40 +0900

    feat: 조회수 업데이트 비관적락 추가로 동시 증가 방어

--- src/main/java/com/example/urltracking/api/repository/dailycount/DailyCountRepository.java
+++ src/main/java/com/example/urltracking/api/repository/dailycount/DailyCountRepository.java
@@ -2,15 +2,17 @@ package com.example.urltracking.api.repository.dailycount;
 
 import com.example.urltracking.entity.dailycount.DailyCount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.LockModeType;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface DailyCountRepository extends JpaRepository<DailyCount, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = "SELECT d FROM DailyCount d WHERE d.trackingUrl = :trackingUrl AND d.date = CURRENT_DATE")
     Optional<DailyCount> findByTrackingUrlWithDate(@Param("trackingUrl") String trackingUrl);
 
--- src/main/java/com/example/urltracking/api/repository/urls/UrlsRepository.java
+++ src/main/java/com/example/urltracking/api/repository/urls/UrlsRepository.java
@@ -2,15 +2,17 @@ package com.example.urltracking.api.repository.urls;
 
 import com.example.urltracking.entity.urls.Urls;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface UrlsRepository extends JpaRepository<Urls,Long> {
     boolean existsByTrackingUrl(String trackingUrl);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Urls> findByTrackingUrl(String trackingUrl);
 
     @Query(value =
